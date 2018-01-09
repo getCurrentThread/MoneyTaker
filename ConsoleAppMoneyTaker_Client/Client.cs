b1d23de51cbd2144b9340dbe8045be90d074798b
DESKTOP-K9IBDAL\jinhyeok <wak8835@gmail.com>
2018-01-10 07:50:29 +0900

    json 통신 테스트를 위한 클라이언트,서버 임의 분리

---
+++ ConsoleAppMoneyTaker_Client/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMoneyTaker_Client
+{
+    class Client
+    {
+        private const string url = "http://localhost";
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ ConsoleAppMoneyTaker_Client/JsonManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMoneyTaker_Client
+{
+    //해당 클래스는 싱글톤으로 구성할 것
+    class JsonManager
+    {
+        private static JsonManager _instance;
+        
+        protected JsonManager()
+        {
+
+        }
+        public static JsonManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_instance)
+                    {
+                        _instance = new JsonManager();
+                        string requestJson = "someJsonRequestString";
+                        WebClient webClient = new WebClient();
+                        webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                        webClient.Encoding = UTF8Encoding.UTF8;
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        //public JsonManager()
+        //{
+        //    string requestJson = "someJsonRequestString";
+        //    WebClient webClient = new WebClient();
+        //    webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+        //    webClient.Encoding = UTF8Encoding.UTF8;
+        //    string responseJSON = webClient.UploadString(url, requestJson);
+        //}
+    }
+}
